5ce890cf4bbfc3bcc0ef9d962ffa93c7561897f8
SamKynaston <samuelkynno@gmail.com>
2023-07-15 05:03:41 -0700

    renamed files

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,4 +1,8 @@
 const {Router} = require("express") //Configure Router for App
 const router = Router()
 
+const {} = require("../controllers/books.controller")
+
+router.get('/all')
+
 module.exports = router
